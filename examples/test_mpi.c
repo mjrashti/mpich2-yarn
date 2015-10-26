ecf5e9f0223a5fa19ddd728396e8117a58e4b278
root <root@compute-1-0.local>
2015-10-26 17:20:51 -0400

    debug

--- examples/test_mpi.c
+++ examples/test_mpi.c
@@ -4,9 +4,10 @@
 int main(int argc, char *argv[]){
 	int rank, nprocs;
 	printf("Starting test MPI program ...\n");
-	MPI_Init(&argc,&argv);
+	/*MPI_Init(&argc,&argv);
 	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
 	MPI_Comm_size(MPI_COMM_WORLD,&nprocs);
 	printf("MPI initialized successfully. I am rank %d out of %d\n",rank,nprocs);
-	MPI_Finalize();
+	MPI_Finalize();*/
+	return 0;
 }
